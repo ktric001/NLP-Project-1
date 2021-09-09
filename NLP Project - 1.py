ac3319009e8f8e97d0bbbecfaafc63078475c5aa
ktric001 <87986240+ktric001@users.noreply.github.com>
2021-09-08 22:19:03 -0500

    Update NLP Project - 1.py

--- NLP Project - 1.py
+++ NLP Project - 1.py
@@ -106,7 +106,7 @@ import re
 exclude_list = ['maps','policies','preferences','accounts','support']
 
 
-# In[100]:
+# In[109]:
 
 
 def strip_unwanted_urls(urls,exclude_list):
@@ -115,21 +115,123 @@ def strip_unwanted_urls(urls,exclude_list):
         if 'https://' in url and not any(exclude_word in url for exclude_word in exclude_list):
             res = re.findall(r'(https?://\S+)', url)[0].split('&')[0]
             val.append(res)
-            print(res)
-        return list(val)
+    return list(val)
 
 
-# In[101]:
+# In[110]:
 
 
 cleaned_urls = {ticker:strip_unwanted_urls(raw_urls[ticker], exclude_list) for ticker in monitored_tickers}
 cleaned_urls
 
 
-# In[102]:
+# In[117]:
 
 
-raw_urls
+def scrape_and_process(URLs):
+    ARTICLES = []
+    for url in URLs:
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text,'html.parser')
+        paragraphs = soup.find_all('p')
+        text = [paragraph.text for paragraph in paragraphs]
+        words = ' '.join(text).split(" ")[:350]
+        ARTICLE = " ".join(words)
+        ARTICLES.append(ARTICLE)
+    return ARTICLES
+
+
+# In[118]:
+
+
+articles = {ticker:scrape_and_process(cleaned_urls[ticker]) for ticker in monitored_tickers}
+articles
+
+
+# In[ ]:
+
+
+def summarize(articles):
+    summaries = []
+    for article in articles:
+        input_ids = tokenizer.encode(article,return_tensors='pt')
+        output = model.generate(input_ids,max_length=55,num_beams=5,early_stopping=True)
+        summary = tokenizer.decode(output[0],skip_special_tokens=True)
+        summaries.append(summary)
+    return summaries
+
+
+# In[121]:
+
+
+summaries = {ticker:summarize(articles[ticker]) for ticker in monitored_tickers}
+summaries
+
+
+# In[122]:
+
+
+from transformers import pipeline
+sentiment = pipeline('sentiment-analysis')
+
+
+# In[124]:
+
+
+scores = {ticker:sentiment(summaries[ticker]) for ticker in monitored_tickers}
+scores
+
+
+# In[125]:
+
+
+print(summaries['BTC'][0], scores['BTC'][0],scores['BTC'][0]['score'])
+
+
+# In[134]:
+
+
+def create_output_array(summaries,scores,urls):
+    output = []
+    for ticker in monitored_tickers:
+        for counter in range(len(summaries[ticker])):
+            output_this = [
+                ticker,
+                summaries[ticker][counter],
+                scores[ticker][counter]['label'],
+                scores[ticker][counter]['score'],
+                urls[ticker][counter]
+            ]
+            output.append(output_this)
+    return output
+
+
+# In[135]:
+
+
+final_output = create_output_array(summaries,scores,cleaned_urls)
+final_output
+
+
+# In[136]:
+
+
+final_output.insert(0,['Ticker','Summary','Label','Confidence','URL'])
+
+
+# In[137]:
+
+
+final_output
+
+
+# In[139]:
+
+
+import csv
+with open('assetsummaries.csv',mode='w',newline='') as f:
+    csv_writer = csv.writer(f,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerows(final_output)
 
 
 # In[ ]:
